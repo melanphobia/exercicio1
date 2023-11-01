9e49f50f2e9cada9eefc32164f5c7a343d78db38
melanphobia <gabrieleuthauser@gmail.com>
2023-11-01 16:42:01 -0300

    Desafio listas

---
+++ lista/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+
+        public string ToString()
+        {
+            return $"id:{Id} nome:{Placa} marca: {Marca} modelo:{Modelo} cor:{Cor}";
+        }
+    }
+}
---
+++ lista/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car() { Id = 1, Placa = "chrevolet" });
+            cars.Add(new Car() { Id = 2, Marca = "citroen" });
+            cars.Add(new Car() { Id = 3, Modelo = "bmw" });
+            cars.Add(new Car() { Id = 4, Cor = "bmw" });
+
+            foreach (Car car in cars)
+            {
+                    Console.WriteLine(car.ToString());
+            }
+            Console.WriteLine("diga o que deseja adicionar: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    int y = 0;
+                    string off = "sim";
+                    while(off == "sim")
+                    {
+                        y++;
+                        Console.WriteLine("insira a placa desejada");
+                        string placa = Console.ReadLine();
+                        Console.WriteLine("insira o marca do carro");
+                        string marca = Console.ReadLine();
+                        Console.WriteLine("insira o modelo do carro");
+                        string modelo = Console.ReadLine();
+                        Console.WriteLine("insira o cor do carro");
+                        string cor = Console.ReadLine();
+                        
+                        cars.Add(new Car() { Id = y, Placa = placa, Modelo = modelo, Marca = marca, Cor = cor});
+
+                        Console.WriteLine("deseja cadastrar algum carro novamente?");
+                        off = Console.ReadLine();
+
+                    }
+                   break;
+
+            }
+        }
+        
+    }
+}
